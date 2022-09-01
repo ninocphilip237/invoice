00c1fb6dd62d2fdb6dbf981651e9858c5692609e
Nino C Philip <ninophilip.techmaven@gmail.com>
2022-09-01 18:41:29 +0530

    discount

--- print_invoice.php
+++ print_invoice.php
@@ -5,8 +5,8 @@ $invoice = new Invoice();
 $invoice->checkLoggedIn();
 if(!empty($_GET['invoice_id']) && $_GET['invoice_id']) {
 	echo $_GET['invoice_id'];
-	$invoiceValues = $invoice->getInvoice($_GET['invoice_id']);		
-	$invoiceItems = $invoice->getInvoiceItems($_GET['invoice_id']);		
+	$invoiceValues = $invoice->getInvoice($_GET['invoice_id']);
+	$invoiceItems = $invoice->getInvoiceItems($_GET['invoice_id']);
 }
 $invoiceDate = date("d/M/Y, H:i:s", strtotime($invoiceValues['order_date']));
 $output = '';
@@ -21,10 +21,10 @@ $output .= '<table width="100%" border="1" cellpadding="5" cellspacing="0">
 	<td width="65%">
 	To,<br />
 	<b>RECEIVER (BILL TO)</b><br />
-	Name : '.$invoiceValues['order_receiver_name'].'<br /> 
+	Name : '.$invoiceValues['order_receiver_name'].'<br />
 	Billing Address : '.$invoiceValues['order_receiver_address'].'<br />
 	</td>
-	<td width="35%">         
+	<td width="35%">
 	Invoice No. : '.$invoiceValues['order_id'].'<br />
 	Invoice Date : '.$invoiceDate.'<br />
 	</td>
@@ -38,9 +38,9 @@ $output .= '<table width="100%" border="1" cellpadding="5" cellspacing="0">
 	<th align="left">Item Name</th>
 	<th align="left">Quantity</th>
 	<th align="left">Price</th>
-	<th align="left">Actual Amt.</th> 
+	<th align="left">Actual Amt.</th>
 	</tr>';
-$count = 0;   
+$count = 0;
 foreach($invoiceItems as $invoiceItem){
 	$count++;
 	$output .= '
@@ -50,7 +50,7 @@ foreach($invoiceItems as $invoiceItem){
 	<td align="left">'.$invoiceItem["item_name"].'</td>
 	<td align="left">'.$invoiceItem["order_item_quantity"].'</td>
 	<td align="left">'.$invoiceItem["order_item_price"].'</td>
-	<td align="left">'.$invoiceItem["order_item_final_amount"].'</td>   
+	<td align="left">'.$invoiceItem["order_item_final_amount"].'</td>
 	</tr>';
 }
 $output .= '
@@ -67,15 +67,15 @@ $output .= '
 	<td align="left">'.$invoiceValues['order_total_tax'].'</td>
 	</tr>
 	<tr>
-	<td align="right" colspan="5">Total: </td>
+	<td align="right" colspan="5">Total with Tax: </td>
 	<td align="left">'.$invoiceValues['order_total_after_tax'].'</td>
 	</tr>
 	<tr>
-	<td align="right" colspan="5">Amount Paid:</td>
+	<td align="right" colspan="5">Discount given:</td>
 	<td align="left">'.$invoiceValues['order_amount_paid'].'</td>
 	</tr>
 	<tr>
-	<td align="right" colspan="5"><b>Amount Due:</b></td>
+	<td align="right" colspan="5"><b>Total to be paid:</b></td>
 	<td align="left">'.$invoiceValues['order_total_amount_due'].'</td>
 	</tr>';
 $output .= '
@@ -83,7 +83,7 @@ $output .= '
 	</td>
 	</tr>
 	</table>';
-// create pdf of invoice	
+// create pdf of invoice
 $invoiceFileName = 'Invoice-'.$invoiceValues['order_id'].'.pdf';
 require_once 'dompdf/src/Autoloader.php';
 Dompdf\Autoloader::register();
@@ -93,5 +93,4 @@ $dompdf->loadHtml(html_entity_decode($output));
 $dompdf->setPaper('A4', 'landscape');
 $dompdf->render();
 $dompdf->stream($invoiceFileName, array("Attachment" => false));
-?>   
-   
+?>
