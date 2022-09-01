3ae01f1c5af21c61fd4d9f8084ee367c811f363a
Nino C Philip <ninophilip.techmaven@gmail.com>
2022-09-01 12:40:50 +0530

    sadsad

--- menu.php
+++ menu.php
@@ -4,17 +4,17 @@
 	<span class="caret"></span></button>
 	<ul class="dropdown-menu">
 		<li><a href="invoice_list.php">Invoice List</a></li>
-		<li><a href="create_invoice.php">Create Invoice</a></li>				  
+		<li><a href="create_invoice.php">Create Invoice</a></li>
 	</ul>
 </li>
-<?php 
+<?php
 if($_SESSION['userid']) { ?>
 	<li class="dropdown">
 		<button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">Logged in <?php echo $_SESSION['user']; ?>
 		<span class="caret"></span></button>
 		<ul class="dropdown-menu">
-			<li><a href="#">Account</a></li>
-			<li><a href="action.php?action=logout">Logout</a></li>		  
+			<!-- <li><a href="#">Account</a></li> -->
+			<li><a href="action.php?action=logout">Logout</a></li>
 		</ul>
 	</li>
 <?php } ?>
